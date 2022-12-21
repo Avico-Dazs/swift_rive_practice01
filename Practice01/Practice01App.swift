bb8a3276f0cd377b0f4a88733f7d50ae6a8d1bad
Avico-Dazs <49014711+Avico-Dazs@users.noreply.github.com>
2022-12-21 22:44:52 +0900

    Initial Commit

---
+++ Practice01/Practice01App.swift
@@ -0,0 +1,17 @@
+//
+//  Practice01App.swift
+//  Practice01
+//
+//  Created by Yusuke Abiko on 2022/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Practice01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
